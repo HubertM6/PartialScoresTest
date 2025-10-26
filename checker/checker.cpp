84d79a04da48efe31874af71a5c832218669c13e
Hubert <h.makowski66@gmail.com>
2025-10-26 20:44:40 +0100

    Updating

--- checker/checker.cpp
+++ checker/checker.cpp
@@ -13,7 +13,7 @@ int main(int argc, char* argv[])
     int expectedValue = ans.readInt();
     int userValue = ouf.readInt();
 
-    if (userValue == expectedValue) quitf(_pc(2), "Output is correct.");
-    else if (abs(userValue - expectedValue) <= 10) quitf(_pc(1), "The absolute difference is within limits for 1 point. Expected %d, found %d.", expectedValue, userValue);
-    else quitf(_pc(0), "Wrong answer. Expected %d, found %d.", expectedValue, userValue);
+    if (userValue == expectedValue) quitp(_pc(2), "Output is correct.");
+    else if (abs(userValue - expectedValue) <= 10) quitp(_pc(1), "The absolute difference is within limits for 1 point. Expected %d, found %d.", expectedValue, userValue);
+    else quitp(_pc(0), "Wrong answer. Expected %d, found %d.", expectedValue, userValue);
 }
